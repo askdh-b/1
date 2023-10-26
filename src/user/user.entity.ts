a983cc39cd60e5b59cce58fe56454aa383a0dcc1
askdh-b <rustpy2703@gmail.com>
2023-10-26 17:09:09 +0600

    added constraints for user entity

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,3 +1,5 @@
+import { Exclude } from "class-transformer";
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
@@ -5,18 +7,32 @@ export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @IsString()
+    @MinLength(4)
+    @MaxLength(40)
     @Column({unique: true})
     username: string;
 
+    @IsEmail()
+    @MinLength(5)
+    @MaxLength(40)
     @Column({unique: true})
     email: string;
 
+    @IsString()
+    @MinLength(2)
+    @MaxLength(40)
     @Column()
     firstname: string;
 
+    @IsString()
+    @MinLength(2)
+    @MaxLength(40)
     @Column()
     lastname: string;
 
+    @IsString()
     @Column()
+    @Exclude({toPlainOnly: true})   
     password_hash: string;
 }
